82cf4ac05b8fd6a3504dbe5dce259b5b84a8de3c
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-06-14 08:47:44 +0100

    refactor: node reference resolution logic

--- dialogue2mermaid/cli.py
+++ dialogue2mermaid/cli.py
@@ -3,6 +3,9 @@ from pprint import pprint as print
 
 
 FALL_THROUGH = 'fallThrough'
+END = -1
+DIALOGUE_STOP = 'dialogue.stop'
+SEPARATOR = "---------------------------------------------------------------------------"
 
 
 def get_dialogue_nodes(dialogue: dict):
@@ -12,80 +15,114 @@ def get_dialogue_nodes(dialogue: dict):
     return json_object.get('nodes')
 
 
-def get_next_node(node: dict):
+def unify_next_node_references(nodes: list):
     """
-    Return the value of the next node as is.
-    Return 'fallThrough' string if no next node defined
-    Return None if the next node is null
+    Update the next node reference to a standard reference.
+    - if no reference and no last node: FALLTHROUGH
+    - if no reference and last node: END
+    - if reference null: END
+    Return the next node value(s).
     """
-    if 'nextNodeIndex' in node:
-        return node.get('nextNodeIndex')
-    elif 'nextNode' in node:
-        return node.get('nextNode')
-    elif 'passNode' in node and 'failNode' in node:
-        return (node.get('passNode'), node.get('failNode'))
-    elif 'passNodeIndex' in node and 'failNodeIndex' in node:
-        return (node.get('passNodeIndex'), node.get('failNodeIndex'))
-    else:
-        return FALL_THROUGH
-
-
-def process_node(node: dict, node_index: int) -> set:
-    # print(str(node))
-    node_type = node.get('type')
-    node_label = node.get('id')
-    result = {
-        "ref": (node_label if node_label is not None else node_index),
-        "index": node_index,
-        "next_node": get_next_node(node)
-    }
-    if node_type == 'message':
-        result.update({'content': 'print'})
-    elif node_type == 'operation':
-        result.update({'content': 'operation'})
-    elif node_type == 'action':
-        result.update({'content': 'action'})
-    elif node_type == 'decision':
-        result.update({'content': 'decision'})
-    elif node_type == 'card':
-        result.update({'content': 'card'})
-    elif node_type == 'customCardCollection':
-        result.update({'content': 'customCardCollection'})
-    else:
-        result.update({'content': 'NOT UNDERSTOOD'})
-    return result
-
-
-def link_node(current_node: dict, nodes: list):
-    next_node = current_node['next_node']
-    result = current_node.copy()
-    if next_node is None:
-        result.update({'next_node': 'END'})
-    elif type(next_node) == int:
-        pass
-    elif type(next_node) == str:
-        if next_node == FALL_THROUGH:
-            result.update({'next_node': current_node['index'] + 1})
+    for node in nodes:
+        if 'nextNodeIndex' in node:
+            node['next'] = node.pop('nextNodeIndex')
+        elif 'nextNode' in node:
+            node['next'] = node.pop('nextNode')
+        elif 'passNode' in node and 'failNode' in node:
+            node.update({'next': {
+                'pass': node.pop('passNode'),
+                'fail': node.pop('failNode'),
+            }})
+        elif 'passNodeIndex' in node and 'failNodeIndex' in node:
+            node.update({'next': {
+                'pass': node.pop('passNodeIndex'),
+                'fail': node.pop('failNodeIndex'),
+            }})
         else:
-            for i, node in enumerate(nodes):
-                if next_node == node['ref']:
-                    result.update({'next_node': i})
+            i = node['index']
+            if i == len(nodes) - 1:
+                node.update({'next': END})      # last node
+            else:
+                node.update({'next': i + 1})    # fall to following node
+        # update stop dialogue references to END
+        current_ref = node['next']
+        if current_ref is None or current_ref == DIALOGUE_STOP:
+            node.update({'next': END})
+    return nodes
+
+
+def indexify_node_label_references(nodes: list):
+    """
+    Change "node label" type next references to indices
+    """
+    for node in nodes:
+        next_node = node['next']
+        if type(next_node) is str:
+            for node2 in nodes:
+                if node2.get('label') == next_node:
+                    node.update({'next': node['index']})
                     break
-    return result
+            continue
+        if type(next_node) is dict:
+            next_node_pass = next_node['pass']
+            next_node_fail = next_node['fail']
+            if type(next_node_pass) is str:
+                for node2 in nodes:
+                    if node2.get('label') == next_node_pass:
+                        next_node_pass = node2['index']
+            if type(next_node_fail) is str:
+                for node2 in nodes:
+                    if node2.get('label') == next_node_fail:
+                        next_node_fail = node2['index']
+            node.update({'next': {
+                'pass': next_node_pass,
+                'fail': next_node_fail
+            }})
+    return nodes
+
+
+def link_nodes(nodes: list) -> str:
+    # Add indices to nodes
+    for i, node in enumerate(nodes):
+        node.update({'index': i})
+    nodes = unify_next_node_references(nodes)
+    nodes = indexify_node_label_references(nodes)
+    return nodes
+
+
+def beautify_nodes(nodes: list) -> list:
+    """
+    Customize each node content and return all nodes.
+    Return each node data as a dictionary.
+    """
+    for node in nodes:
+        node_type = node.get('type')
+        if node_type == 'message':
+            node.update({'content': 'print'})
+        elif node_type == 'operation':
+            node.update({'content': 'operation'})
+        elif node_type == 'action':
+            node.update({'content': 'action'})
+        elif node_type == 'decision':
+            node.update({'content': 'decision'})
+        elif node_type == 'card':
+            node.update({'content': 'card'})
+        elif node_type == 'customCardCollection':
+            node.update({'content': 'customCardCollection'})
+        else:
+            node.update({'content': 'NOT UNDERSTOOD'})
+    return nodes
+
+
+def stringify_nodes(nodes: list) -> list:
+    return nodes
 
 
 def nodes_to_mermaid(nodes: list) -> str:
-    round1 = []
-    for i, node in enumerate(nodes):
-        output = process_node(node, i)
-        round1.append(output)
-        print(f"{output['ref']} @ {output['content']} -> {output['next_node']}")
-    round2 = []
-    for node in round1:
-        output = link_node(node, round1)
-        round2.append(output)
-    return round2
-    # return str(round2)
+    linked_nodes = link_nodes(nodes)
+    beautified_nodes = beautify_nodes(linked_nodes)
+    stringified_nodes = stringify_nodes(beautified_nodes)
+    return stringified_nodes
 
 
 def main():
